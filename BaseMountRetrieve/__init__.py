4a6ac630894afb0a81b65c1b87159d71f25103a5
bfssi-forest-dussault <forest.dussault@canada.ca>
2018-11-28 13:34:25 -0500

    Removed junk

--- BaseMountRetrieve/__init__.py
+++ BaseMountRetrieve/__init__.py
@@ -1,15 +1,3 @@
 __version__ = "0.5.3"
 __author__ = "Forest Dussault"
 __email__ = "forest.dussault@canada.ca"
-
-
-def get_phenotype(gene: str):
-    return gene
-
-
-def pipeline(gene_list: list) -> list:
-    phenotype_list = []
-    for gene in gene_list:
-        phenotype = get_phenotype(gene)
-        phenotype_list.append(phenotype)
-    return phenotype_list
